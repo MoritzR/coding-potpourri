b16d00faf989836620b67479d05dbeb4b5049f28
Moritz Rumpf <moritz.rumpf@deutsche-fensterbau.de>
2022-04-01 16:24:41 +0200

    Add pattern matching syntax examples

--- basics/0_syntax.hs
+++ basics/0_syntax.hs
@@ -21,38 +21,49 @@ ifElse bool ifBranch elseBranch =
 -- guards (if elif elif else)
 somethingWithNumber :: Int -> String
 somethingWithNumber number
-    | number == 0 = "number is equal to 0"
-    | number == 4 = "number is equal to 4"
-    | otherwise = "number is something else"
+  | number == 0 = "number is equal to 0"
+  | number == 4 = "number is equal to 4"
+  | otherwise = "number is something else"
 
 -- let
-aPlusBTimesC :: Int -> Int-> Int-> Int
+aPlusBTimesC :: Int -> Int -> Int -> Int
 aPlusBTimesC a b c =
-    let
-        aPlusB = a + b
-    in
-        aPlusB * c
+  let aPlusB = a + b
+   in aPlusB * c
 
 -- where
-aPlusBTimesC' :: Int -> Int-> Int-> Int
+aPlusBTimesC' :: Int -> Int -> Int -> Int
 aPlusBTimesC' a b c = aPlusB + c
-    where aPlusB = a + b
+  where
+    aPlusB = a + b
+
+-- pattern matching
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n -1) + fib (n -2)
+
+orElse :: Maybe a -> a -> a
+orElse (Just a) _ = a
+orElse Nothing a = a
 
 -- list comprehensions
-oneToTen = [1..10]
-evensFromOneToTen = [2,4..10]
-oneToFive = [ i | i <- [1..10], i <= 5]
-twoToSix = [ i + 1 | i <- [1..10], i <= 5]
+oneToTen = [1 .. 10]
+
+evensFromOneToTen = [2, 4 .. 10]
+
+oneToFive = [i | i <- [1 .. 10], i <= 5]
+
+twoToSix = [i + 1 | i <- [1 .. 10], i <= 5]
 
 -- function composition
-plus2Times3 = (*3) . (+2)
+plus2Times3 = (* 3) . (+ 2)
 
 -- pipeline notation
 sumEvens :: [Int] -> Int
 sumEvens numbers =
-    numbers
-        & filter even
-        & foldr add 0
+  numbers
+    & filter even
+    & foldr add 0
 
 -- type holes
 -- sumEvens' :: [Int] -> Int
@@ -63,4 +74,5 @@ sumEvens numbers =
 
 -- infix notation
 plus = add
+
 onePlusTwo = 1 `plus` 2
