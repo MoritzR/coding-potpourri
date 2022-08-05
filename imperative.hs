46d47512ac0ac329d2f26d03e794405838ecd400
Moritz Rumpf <moritz.rumpf@deutsche-fensterbau.de>
2022-08-05 15:59:55 +0200

    Simplify imperative

--- imperative.hs
+++ imperative.hs
@@ -1,9 +1,8 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE RebindableSyntax #-}
-{-# LANGUAGE TypeFamilies #-}
 
 import Control.Monad (when)
-import Control.Monad.State (MonadState, State, get)
+import Control.Monad.State (MonadState, State)
 import Prelude hiding (return, (>>), (>>=))
 
 data New = New
@@ -12,49 +11,43 @@ data IfRan = IfRan | IfDidn'tRun
 
 data IndexedState stateBefore stateAfter result = IndexedState {runIndexedState :: stateBefore -> (stateAfter, result)}
 
-ireturn :: a -> IndexedState s s a
-ireturn a = IndexedState $ \s -> (s, a)
+get :: IndexedState p p p
+get = IndexedState $ \s -> (s, s)
 
-ibind :: IndexedState p q a -> (a -> IndexedState q r b) -> IndexedState p r b
-ibind state f = IndexedState $ \p ->
-  let (q, a) = runIndexedState state p
+put :: so -> IndexedState si so ()
+put x = IndexedState (\si -> (x, ()))
+
+(>>=) :: IndexedState p q a -> (a -> IndexedState q r b) -> IndexedState p r b
+s >>= f = IndexedState $ \p ->
+  let (q, a) = runIndexedState s p
       (r, b) = runIndexedState (f a) q
    in (r, b)
 
-iget :: IndexedState p p p
-iget = IndexedState $ \s -> (s, s)
-
-iput :: so -> IndexedState si so ()
-iput x = IndexedState (\si -> (x, ()))
-
-(>>=) :: IndexedState p q a -> (a -> IndexedState q r b) -> IndexedState p r b
-(>>=) = ibind
-
 return :: a -> IndexedState s s a
-return = ireturn
+return a = IndexedState $ \s -> (s, a)
 
 (>>) :: IndexedState p q a -> IndexedState q r b -> IndexedState p r b
 s1 >> s2 = s1 >>= \_ -> s2
 
 if' :: Bool -> IndexedState a b () -> IndexedState a IfRan ()
-if' True action = action >> iput IfRan
-if' False action = iput IfDidn'tRun
+if' True action = action >> put IfRan
+if' False action = put IfDidn'tRun
 
 else' :: IndexedState IfRan b () -> IndexedState IfRan New ()
 else' action = do
-  ifRan <- iget
+  ifRan <- get
   go ifRan
   where
-    go IfRan = iput New
-    go IfDidn'tRun = action >> iput New
+    go IfRan = put New
+    go IfDidn'tRun = action >> put New
 
 elif' :: Bool -> IndexedState IfRan b () -> IndexedState IfRan IfRan ()
 elif' True action = do
-  ifRan <- iget
+  ifRan <- get
   go ifRan
   where
     go IfRan = return ()
-    go IfDidn'tRun = action >> iput IfRan
+    go IfDidn'tRun = action >> put IfRan
 
 def :: IndexedState New b a -> a
 def state = snd $ runIndexedState state New
@@ -69,7 +62,7 @@ prog1 = def do
 
   elif' True do
     return ()
-    
+
   else' do
     return ()
 
