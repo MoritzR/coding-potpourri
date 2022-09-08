592491a584346124fee753eaab46bb19bd8d5be1
Moritz Rumpf <moritz.rumpf@deutsche-fensterbau.de>
2022-09-08 12:24:12 +0200

    Formatting

--- typing-in-anger/overloads.ts
+++ typing-in-anger/overloads.ts
@@ -32,29 +32,29 @@ const callOverload2 = call(overload, "PARAM2")
  * should not compile, but it does and throws a runtime error
  */
 
- function overloadWithOneOrTwoParameters(number1: number, number2: number): number
- function overloadWithOneOrTwoParameters(number: number): string
- function overloadWithOneOrTwoParameters(number1: number, optionalNumber?: number): string | number {
-     if (optionalNumber !== undefined) {
-         return number1 + optionalNumber
-     }
-     return number1.toString()
- }
- 
- function call2<A, B, C>(fn: ((a: A, b: B) => C), input1: A, input2: B) {
-     return fn(input1, input2)
- }
- 
- /**
-  * infered as string, but is actually a number
-  * this is because it picks the one parameter overload, even though call2 expects a two parameter callback
-  * addtionally: call2 is inferred as
-  *     `call2<1, number, string>(fn: (a: 1, b: number) => string, input1: 1, input2: number): string`
-  * and I am not sure why one parameter is infered as a literal `1` while the other is `number`
-  */
- const aNumber = call2(overloadWithOneOrTwoParameters, 1, 2)
- 
- // runtime error! g.split is not a function
- console.log(aNumber.split(""))
+function overloadWithOneOrTwoParameters(number1: number, number2: number): number
+function overloadWithOneOrTwoParameters(number: number): string
+function overloadWithOneOrTwoParameters(number1: number, optionalNumber?: number): string | number {
+    if (optionalNumber !== undefined) {
+        return number1 + optionalNumber
+    }
+    return number1.toString()
+}
+
+function call2<A, B, C>(fn: ((a: A, b: B) => C), input1: A, input2: B) {
+    return fn(input1, input2)
+}
+
+/**
+ * infered as string, but is actually a number
+ * this is because it picks the one parameter overload, even though call2 expects a two parameter callback
+ * addtionally: call2 is inferred as
+ *     `call2<1, number, string>(fn: (a: 1, b: number) => string, input1: 1, input2: number): string`
+ * and I am not sure why one parameter is infered as a literal `1` while the other is `number`
+ */
+const aNumber = call2(overloadWithOneOrTwoParameters, 1, 2)
+
+// runtime error! g.split is not a function
+console.log(aNumber.split(""))
 
  
