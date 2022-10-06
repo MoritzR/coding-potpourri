e89b1127ba3be7f8b4dcc0ba5113586636f4edbd
Moritz Rumpf <moritz.rumpf@deutsche-fensterbau.de>
2022-10-06 10:53:59 +0200

    Add a typing in anger type predicate example

---
+++ typing-in-anger/typePredicate.ts
@@ -0,0 +1,25 @@
+interface A {
+    a: "A"
+}
+
+interface AB extends A {
+    b: "B"
+}
+
+function isAB1(
+    a: A
+): a is AB {
+    return 'b' in a;
+}
+
+const isAB2 = (a: A): a is AB => 'b' in a
+
+type TypePredicate<A, B extends A> = (
+    input: A
+) => input is B
+
+const isAB3: TypePredicate<A, AB> = (a: A): a is AB => 'b' in a
+
+// This errors because it is missing the specific 'a is AB'
+// predicate, even though it is contained in TypePredicate<>
+const isAB4: TypePredicate<A, AB> = (a: A) => 'b' in a
