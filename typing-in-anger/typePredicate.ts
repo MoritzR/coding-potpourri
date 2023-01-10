5ab01055c30f92490de14dfcb0bd424a02b44d5f
Moritz Rumpf <moritz.rumpf@gmail.com>
2023-01-10 16:39:41 +0100

    Remove unnecessary type annotation

--- typing-in-anger/typePredicate.ts
+++ typing-in-anger/typePredicate.ts
@@ -18,7 +18,7 @@ type TypePredicate<A, B extends A> = (
     input: A
 ) => input is B
 
-const isAB3: TypePredicate<A, AB> = (a: A): a is AB => 'b' in a
+const isAB3: TypePredicate<A, AB> = (a): a is AB => 'b' in a
 
 // This errors because it is missing the specific 'a is AB'
 // predicate, even though it is contained in TypePredicate<>
