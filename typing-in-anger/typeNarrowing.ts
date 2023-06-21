8d1f13859d69a7871e5165126f8187f3858367f2
Moritz Rumpf <moritz.rumpf@gmail.com>
2023-06-21 08:18:02 +0200

    Move example

--- typing-in-anger/typeNarrowing.ts
+++ typing-in-anger/typeNarrowing.ts
@@ -9,3 +9,16 @@ const outer = () => {
 
     const callback = () => foo.a.toFixed() // foo.a is no longer a number, but why?
 }
+
+
+const f1 = (x: number | string | boolean): x is number => typeof x === "number"
+const f2 = (x: number | string | boolean): x is string => typeof x === "string"
+const f = f1 as typeof f1 | typeof f2
+const n = "hi" as number | string | boolean
+
+if (f(n)) {
+    console.log(n)
+    n.valueOf
+} else {
+    console.log(n) // n should NOT be a boolean
+}
--- typing-in-anger/typePredicate.ts
+++ typing-in-anger/typePredicate.ts
@@ -1,16 +1,3 @@
-const f1 = (x: number | string | boolean): x is number => typeof x === "number"
-const f2 = (x: number | string | boolean): x is string => typeof x === "string"
-const f = f1 as typeof f1 | typeof f2
-const n = "hi" as number | string | boolean
-
-if (f(n)) {
-    console.log(n)
-    n.valueOf
-} else {
-    console.log(n) // n should NOT be a boolean
-}
-
-
 interface A {
     a: "A"
 }
