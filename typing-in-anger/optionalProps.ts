5a9992839c91fc83650c282529ab981d40d84d94
Moritz Rumpf <moritz.rumpf@gmail.com>
2023-07-10 10:53:15 +0200

    Add optional props example
    Optional props can cause runtime errors that TypeScript doesn't catch

---
+++ typing-in-anger/optionalProps.ts
@@ -0,0 +1,14 @@
+// Optional properties can cause runtime errors.
+
+type Foo = { readonly foo: string };
+type Bar = { readonly foo: string, bar?: number };
+// This won't compile
+// const foo: Foo = { foo: "foo", bar: "bar" };
+// But this will
+const thing = { foo: "foo", bar: "bar" };
+const foo: Foo = thing;
+// Uh oh...
+const bar: Bar = foo;
+
+// runtime error :(
+bar.bar?.toFixed()
