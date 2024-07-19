9d17f725447536eb27ea62706acc01541070fd49
Moritz Rumpf <moritz.rumpf@gmail.com>
2024-07-19 16:01:55 +0200

    Remove expamble
    This makes sense because the value and therefor type could change by the time the function is called.

--- typing-in-anger/typeNarrowing.ts
+++ typing-in-anger/typeNarrowing.ts
@@ -1,16 +1,3 @@
-const foo: { readonly a: string | number } = { a: "hello world" }
-
-const outer = () => {
-    if (typeof foo.a === "string") {
-        return
-    }
-
-    foo.a.toFixed() // foo.a is a number
-
-    const callback = () => foo.a.toFixed() // foo.a is no longer a number, but why?
-}
-
-
 const f1 = (x: number | string | boolean): x is number => typeof x === "number"
 const f2 = (x: number | string | boolean): x is string => typeof x === "string"
 const f = f1 as typeof f1 | typeof f2
