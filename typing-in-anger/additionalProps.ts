5e142977e02f697ece54af032aeb641af4e372c3
Moritz Rumpf <moritz.rumpf@gmail.com>
2024-07-22 15:43:00 +0200

    Update example
    to give a better use case with the renaming of a property

--- typing-in-anger/additionalProps.ts
+++ typing-in-anger/additionalProps.ts
@@ -1,16 +1,16 @@
 // Let's run a web app. It need some settings for starting the app.
+declare function startApp(startupSettings: Partial<Settings>): void
 
 type Settings = {
     waitForDebugger: boolean,
     logLevel: "debug" | "warn" | "info" | "error"
+    developerMode: boolean
 }
-
-const startApp = (startupSettings: Settings) => console.log("App starting...", startupSettings)
 
 startApp({
     waitForDebugger: false,
     logLevel: "warn",
-    developerMode: true // oh, I guess there is no developer mode, good that typescript complains
+    devMode: true // oh, devMode was renamed to developerMode, good that typescript complains
 })
 
 // Let's do the same but now our app is able to start with different settings based on the environment
@@ -18,7 +18,7 @@ startApp({
 type Env = "production" | "development"
 
 // the environment is provided by the app startup process itself
-const startAppEnvironmentSpecific = (getSettings: (env: Env) => Settings) => {
+const startAppEnvironmentSpecific = (getSettings: (env: Env) => Partial<Settings>) => {
     const env: Env = "development" // get Env from somewhere
     const settings = getSettings(env)
     console.log("App starting...", settings)
@@ -27,7 +27,7 @@ const startAppEnvironmentSpecific = (getSettings: (env: Env) => Settings) => {
 startAppEnvironmentSpecific(env => ({
     logLevel: "warn",
     waitForDebugger: false,
-    /* Now typescript doesn't complain, it looks like we are setting a developer mode,
+    /* Now typescript doesn't complain, it looks like we are setting a dev mode,
         but in fact, there is no such thing! */
-    developerMode: env === "development" // no typescript error :(
+    devMode: env === "development" // no typescript error :(
 })) // we can fix this by adding 'satisfies Settings'
