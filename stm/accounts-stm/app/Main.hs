e9bfaeb5568695dee4bfb056a8bdd98e5fed3058
Moritz Andrich <moritz.rumpf@gmail.com>
2026-06-16 21:29:07 +0200

    STM: more concurrency

--- stm/accounts-stm/app/Main.hs
+++ stm/accounts-stm/app/Main.hs
@@ -1,12 +1,16 @@
 -- import System.IO
-import Control.Concurrent.STM (TVar, readTVar, writeTVar, newTVar, STM, retry, atomically, check, newTVarIO)
-import Control.Concurrent (threadDelay)
+import Control.Concurrent.STM (TVar, readTVar, writeTVar, STM, retry, atomically, check, newTVarIO)
+import Control.Concurrent.Async (forConcurrently_)
+import Control.Exception (Exception)
+import Control.Monad.STM (throwSTM)
+import Control.Monad (unless)
 
 type Account = TVar Int
 
 withdraw :: Account -> Int -> STM ()
 withdraw acc amount = do
     bal <- readTVar acc
+    checkAtLeast bal amount
     check $ bal >= amount
     writeTVar acc (bal - amount)
 
@@ -16,15 +20,25 @@ transfer from to amount = do
   withdraw from amount
   deposit to amount
 
-test = do
-  acc1 <- newTVar 200
-  acc2 <- newTVar 120
+test acc1 acc2 = do
   withdraw acc1 140 -- acc1: 60
   transfer acc1 acc2 50 -- acc1: 10, acc2: 170
   -- transfer acc1 acc2 20 -- acc1: -10
 
 main = do
-    atomically test
+  acc1 <- newTVarIO 200
+  acc2 <- newTVarIO 120
+  times 2 do
+    atomically $ test acc1 acc2
+    print "done"
+
+
+data AccountException = InsufficentFunds Int Int
+instance Exception AccountException
+instance Show AccountException where
+  show (InsufficentFunds has needs) = "Account has " ++ show has ++ " funds, but needs at least " ++ show needs ++ "."
+
+checkAtLeast bal amount = unless (bal >= amount) $ throwSTM $ InsufficentFunds bal amount
 
 
 
@@ -40,9 +54,7 @@ main = do
 
 
 
-
-
-
+times i action = forConcurrently_ [(1 :: Int) .. i] (const action)
 
 check2 theBool =
   if theBool then return ()
