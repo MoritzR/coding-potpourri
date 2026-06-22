3cb377229d1fd29305a2a375b657036c3ab5e0e6
Moritz Andrich <moritz.rumpf@gmail.com>
2026-06-22 21:58:49 +0200

    Add backup account to withdraw

--- stm/accounts-stm/app/Main.hs
+++ stm/accounts-stm/app/Main.hs
@@ -1,44 +1,33 @@
--- import System.IO
-import Control.Concurrent.STM (TVar, readTVar, writeTVar, STM, retry, atomically, check, newTVarIO)
+import Control.Concurrent.STM (TVar, readTVar, writeTVar, STM, retry, atomically, check, newTVarIO, orElse)
 import Control.Concurrent.Async (forConcurrently_)
 import Control.Exception (Exception)
 import Control.Monad.STM (throwSTM)
-import Control.Monad (unless)
 
 type Account = TVar Int
 
 withdraw :: Account -> Int -> STM ()
 withdraw acc amount = do
     bal <- readTVar acc
-    checkAtLeast bal amount
     check $ bal >= amount
     writeTVar acc (bal - amount)
 
 deposit account amount = withdraw account (-amount)
 
-transfer from to amount = do
-  withdraw from amount
+transfer from backup to amount = do
+  withdraw from amount `orElse` withdraw backup amount
   deposit to amount
-
-test acc1 acc2 = do
-  withdraw acc1 140 -- acc1: 60
-  transfer acc1 acc2 50 -- acc1: 10, acc2: 170
-  -- transfer acc1 acc2 20 -- acc1: -10
 
 main = do
   acc1 <- newTVarIO 200
   acc2 <- newTVarIO 120
-  times 2 do
-    atomically $ test acc1 acc2
+  backup <- newTVarIO 10000
+  times 300 do
+    atomically $ transfer acc1 backup acc2 50 `orElse` throwSTM InsufficentFunds 
     print "done"
 
-
-data AccountException = InsufficentFunds Int Int
+data AccountException = InsufficentFunds deriving Show
 instance Exception AccountException
-instance Show AccountException where
-  show (InsufficentFunds has needs) = "Account has " ++ show has ++ " funds, but needs at least " ++ show needs ++ "."
 
-checkAtLeast bal amount = unless (bal >= amount) $ throwSTM $ InsufficentFunds bal amount
 
 
 
@@ -57,5 +46,6 @@ checkAtLeast bal amount = unless (bal >= amount) $ throwSTM $ InsufficentFunds b
 times i action = forConcurrently_ [(1 :: Int) .. i] (const action)
 
 check2 theBool =
-  if theBool then return ()
+  if theBool
+  then return ()
   else retry
